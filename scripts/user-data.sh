61416859c916eed8baccb250501d34b1c2daf537
hanin18010 <71489404+hanin18010@users.noreply.github.com>
2026-03-17 15:32:31 +0200

    Update user-data.sh

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+sudo apt update -y
+sudo apt install nginx -y
+
+echo "<h1>Welcome to Scalable AWS Web App</h1>" > /var/www/html/index.html
+
+systemctl start nginx
+systemctl enable nginx
